8cc5bebd93a84f0e45764ec3f47263579fe546ae
DorinBe <dorinbeery@gmail.com>
2024-02-23 18:06:59 +0200

    remove unecessary files

--- gui/ParseJsons/edit_json_gui_rooms.py
+++
@@ -1,7 +0,0 @@
-def draw_boxes(data, canvas):
-    # delete all items on canvas
-    canvas.delete("edge")
-    canvas.delete("box")
-    for box in data["boxes"]:
-        x1, y1, x2, y2 = box
-        canvas.create_rectangle(x1, y1, x2, y2, outline="black", width=2, tags="box")
