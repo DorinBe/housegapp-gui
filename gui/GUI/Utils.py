565507caa42c31c6cb605602f461dfa653144b2f
DorinBe <dorinbeery@gmail.com>
2024-04-24 14:12:29 +0300

    remove this exception printing

--- gui/GUI/Utils.py
+++ gui/GUI/Utils.py
@@ -7,4 +7,4 @@ def find_tag(tag_name:str, tag_tuple:tuple):
     for tag in tag_tuple:
         if tag_name in tag:
             return tag.split(":")[1]
-    raise Exception(f"Tag {tag_name} not found")
+    # raise Exception(f"Tag {tag_name} not found") # for debugging only
